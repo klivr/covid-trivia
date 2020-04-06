b6a75eca0fa9ece28beadce479b101fbfee14bed
Benjamin Simon <b3nshi@gmail.com>
2020-04-06 16:06:08 +0200

    Adding cookie consent form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import CookieConsent from "react-cookie-consent";
 
 import HomePage from "./pages/home";
 
-const App: React.FC = () => <HomePage />;
+import styles from "./styles.module.css";
+
+const App: React.FC = () => {
+  const [cookies, setCookies] = useState(false);
+  return (
+    <>
+      <CookieConsent
+        buttonText="Seguir navegando!"
+        location="bottom"
+        onAccept={() => {
+          setCookies(true);
+        }}
+      >
+        Este sitio web utiliza cookies para optimizar la experiencia de usuario.
+      </CookieConsent>
+
+      {!cookies && <div className={styles.overlay} />}
+      <HomePage />
+    </>
+  );
+};
 
 export default App;
