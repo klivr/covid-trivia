25aa77282ab92001be6dd355289f0e852ff5d78c
Benjamin Simon <b3nshi@gmail.com>
2020-04-16 20:03:35 +0200

    Updates

---
+++ precache-manifest.c6f61e66fa087fa74b1d3e66c2d3440f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3d364fa97b1a58aed1f09e14d31c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a8427f2e15582aa536",
+    "url": "/static/css/main.f463443e.chunk.css"
+  },
+  {
+    "revision": "1b6df03745dad5bb50c9",
+    "url": "/static/js/2.57eeb9bb.chunk.js"
+  },
+  {
+    "revision": "92eb7edc341817a708e109b64747c0b9",
+    "url": "/static/js/2.57eeb9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a8427f2e15582aa536",
+    "url": "/static/js/main.2d1e845a.chunk.js"
+  },
+  {
+    "revision": "47e8696993abd58a0d3b",
+    "url": "/static/js/runtime-main.1059f6fa.js"
+  }
+]);
